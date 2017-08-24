7e1d62d7078b7cf0510793d444b97dc1c6d16f3a
luke kaalim <luke@kaal.im>
2017-08-24 15:56:05 +1000

    Add comma

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -10,7 +10,7 @@ const App = () =>
         <SessionCard
             date={new Date(2017, 7, 29)}
             name="Dungeons and Dragons, Season 2"
-            shortDesc="Game run by Luke next Tuesday in Rome"
+            shortDesc="Game run by Luke, next Tuesday in Rome"
             foodStatus="Contact #pen-and-paper for pizza choices/requests"
             color="rgb(193, 253, 215)"
             />
